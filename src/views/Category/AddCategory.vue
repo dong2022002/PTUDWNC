e080c43417f72879bfd885dd00809fb22c11b384
TranDieuDong <dong200220@gmail.com>
2023-03-20 23:25:04 +0700

    add and fix package axios

--- src/views/Category/AddCategory.vue
+++ src/views/Category/AddCategory.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="text-center">
-        <h3>Add Category</h3>
+      <div class="col-12 text-center">
+        <h3 class="pt-3">Add Category</h3>
       </div>
     </div>
     <div class="row">
-      <form action="">
-        <div class="form-group">
-          <label>Category Name</label>
-          <input type="text" class="form-control" />
-        </div>
-        <div class="form-group">
-          <label>Category Description</label>
-          <input type="text" class="form-control" />
-        </div>
-        <div class="form-group">
-          <label>Category Image</label>
-          <input type="text" class="form-control" />
-        </div>
-      </form>
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form action="">
+          <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" v-model="categoryName" />
+          </div>
+          <div class="form-group">
+            <label>Description</label>
+            <textarea
+              type="text"
+              class="form-control"
+              v-model="description"
+            ></textarea>
+          </div>
+          <div class="form-group">
+            <label>Image</label>
+            <input type="text" class="form-control" v-model="imageUrl" />
+          </div>
+          <button type="submit" class="btn btn-prima">Submit</button>
+        </form>
+      </div>
+      <div class="col-3"></div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  setup() {},
+  data() {
+    return {
+      categoryName: "",
+      description: "",
+      imageUrl: "",
+    };
+  },
 };
 </script>
 <style scoped></style>
