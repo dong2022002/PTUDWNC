c81813983603cd2bedd8778245b6dde7e84e376f
TranDieuDong <dong200220@gmail.com>
2023-03-20 22:42:45 +0700

    create project and setup

---
+++ src/views/Category/AddCategory.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="text-center">
+        <h3>Add Category</h3>
+      </div>
+    </div>
+    <div class="row">
+      <form action="">
+        <div class="form-group">
+          <label>Category Name</label>
+          <input type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label>Category Description</label>
+          <input type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label>Category Image</label>
+          <input type="text" class="form-control" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style scoped></style>
