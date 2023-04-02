58c459c81c20ba58f025e1e3d6deaed4df2b9c23
TranDieuDong <dong200220@gmail.com>
2023-04-03 01:00:23 +0700

    update Source Server

---
+++ src/server/ServerShop/Shop.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TatBlog.Data.Contexts
+{
+    public class BlogDbContext :DbContext
+    {
+     
+
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }U            //modelBuilder.ApplyConfigurationsFromAssembly(
+            //    typeof(CategoryMap).Assembly);
+        }
+    }
+}
---
+++ src/server/ServerShop/Shop.Services/Extensions/ConvertString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TatBlog.Services.Extensions
+{
+	public static class ConvertString
+	{
+		public static string GenerateSlug(this string value)
+		{
+			string str = value.RemoveAccent().ToLower();
+			// invalid chars           
+			str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+			// convert multiple spaces into one space   
+			str = Regex.Replace(str, @"\s+", " ").Trim();
+			// cut and trim 
+			str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+			str = Regex.Replace(str, @"\s", "-"); // hyphens   
+			return str;
+
+		}
+		public static string RemoveAccent(this string txt)
+		{
+			byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+			return System.Text.Encoding.ASCII.GetString(bytes);
+		}
+		public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Func<TSource, bool> predicate)
+		{
+			if (condition)
+				return (IQueryable<TSource>)source.Where(predicate);
+			else
+				return source;
+		}
+
+		public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, bool condition, Func<TSource, int, bool> predicate)
+		{
+			if (condition)
+				return source.Where(predicate);
+			else
+				return source;
+		}
+
+	}
+}
